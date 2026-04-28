421c01da3c7baf2ec927d5314d2a2bd1d565e26e
Jon Carifio <carifio24@gmail.com>
2026-04-28 11:01:16 -0400

    Increase far plane value.

--- src/threeWWT.ts
+++ src/threeWWT.ts
@@ -41,7 +41,7 @@ function createDummyCanvas(canvas: HTMLCanvasElement): HTMLCanvasElement {
 }
 
 export function createTHREECamera(renderContext: RenderContext): PerspectiveCamera {
-  const camera = new PerspectiveCamera(75, renderContext.width / renderContext.height, renderContext.nearPlane, 1);
+  const camera = new PerspectiveCamera(75, renderContext.width / renderContext.height, renderContext.nearPlane, 1000);
   camera.matrixAutoUpdate = false;
   return camera;
 }
